30579e27caeecff90850aa3aa33aead920c5f465
younesse boualam <boualambg92@gmail.com>
2024-09-17 05:07:11 +0100

    navbar configuration in progress

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-
-import './App.css';
-import "bootstrap/dist/css/bootstrap.min.css"
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import NavBar from "./components/NavBar";
 function App() {
   return (
-    <h1>hello world</h1>
+    <div>
+      <NavBar />
+    </div>
   );
 }
 
