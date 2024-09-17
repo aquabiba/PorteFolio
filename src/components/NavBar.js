f80d9d8dcc4c77c207c8df763c3d17aea558e210
younesse boualam <boualambg92@gmail.com>
2024-09-17 18:21:54 +0100

    navebar and faontawsome dependencies check

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,7 +5,7 @@ const NavBar = () => {
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-dark">
         <div className="container-fluid">
-          <a style={{ color: "white" }} className="navbar-brand" href="#">
+          <a className="navbar-brand" href="#">
             <img className="logo" src={logo} alt="logo..." />
           </a>
           <button
@@ -21,14 +21,34 @@ const NavBar = () => {
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="#">
+              <li className="nav-item active">
+                <a className="nav-link " aria-current="page" href="#">
                   Home
                 </a>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#">
-                  Link
+                  about me
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Services
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  how work
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Portfolio
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Contacts
                 </a>
               </li>
             </ul>
